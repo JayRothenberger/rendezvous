e5cad2bf8e005cff1cb8aed2826a9c3fbd84a04a
JayRoth-sch <35668950+JayRothenberger@users.noreply.github.com>
2022-02-04 09:35:27 -0600

    Update rendezvous.py

--- rendezvous.py
+++ rendezvous.py
@@ -75,7 +75,7 @@ def main():
                 logger.info(f'connected: {client}, {server}')
         for peer_ip, peer_port, peer_private in peers_to_match:
             sock.sendto('none yet'.encode('ascii'), (peer_ip, peer_port))
-            logger.info(f'sending wait message: {peer_ip[0]}:{peer_port[1]}')
+            logger.info(f'sending wait message: {peer_ip}:{peer_port}')
 
 
 if __name__ == "__main__":
